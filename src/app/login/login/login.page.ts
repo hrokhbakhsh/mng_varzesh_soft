d165dbdf25d84d9d22d2c3b5f99d328b490f5ca4
davood rokhbakhsh <davood.shiraz@gmail.com>
2020-07-11 13:01:12 +0430

    seven/11

--- src/app/login/login/login.page.ts
+++ src/app/login/login/login.page.ts
@@ -1,36 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppServiceService} from '../../services/app-service.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  private form: FormGroup;
+    private form: FormGroup;
+    loading: boolean = false;
+    sSubmit = false;
+
+    constructor(private service: AppServiceService, private fb: FormBuilder) {
+
+        this.form = this.fb.group({
+            username: ['', [Validators.required]],
+            password: ['', [Validators.required]]
+        });
+    }
+
+    ngOnInit() {
+
+    }
 
 
-  constructor(private service: AppServiceService , private fb: FormBuilder) {
-
-    this.form = this.fb.group({
-      username: ['', [Validators.required ]] ,
-      password: ['', [Validators.required ]]
-    });
-  }
-
-  ngOnInit() {
-
-  }
-
-
-  getIp() {
-    console.log(this.service.packageUrl) ;
-
-  }
-
-  login() {
-    console.log(this.form.value.password);
-  }
+    async login() {
+        // localStorage('url');
+        this.sSubmit = true;
+        if (this.form.valid) {
+            try {
+                this.loading = true;
+                const res = await this.service.authUser(this.form).toPromise();
+                if (res.status) {
+                    console.log(res.status);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
 }
--- src/app/services/app-service.service.ts
+++ src/app/services/app-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ResponseIpModel} from './response-ip-model';
 import {HttpClient} from '@angular/common/http';
@@ -6,26 +6,34 @@ import {appconfig} from '../appconfig';
 import {IpModel} from './ip-model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AppServiceService {
 
-  packageUrl: string = null;
+    packageUrl: string = null;
 
-  constructor(private http: HttpClient) { }
-
-  getIp(): Observable<ResponseIpModel> {
-    return this.http.get<ResponseIpModel>(`${appconfig.api}`);
-  }
-
-   async storeUrl(id){
-    try {
-
-      let res = await this.getIp().toPromise();
-      let ipModel: IpModel = res.data[id];
-      this.packageUrl = ipModel.ServerAddress ;
-    }catch (e) {
-
+    constructor(private http: HttpClient) {
     }
-  }
+
+    getIp(): Observable<ResponseIpModel> {
+        return this.http.get<ResponseIpModel>(`${appconfig.api}`);
+    }
+
+
+    authUser(data: any): Observable<any> {
+        const url = 'https://cors-anywhere.herokuapp.com/' + localStorage.getItem('url');
+        return this.http.get(`${url}/mng_login`, data);
+    }
+
+    async storeUrl(id) {
+        try {
+
+            const res = await this.getIp().toPromise();
+            const ipModel: IpModel = res.data[id];
+            this.packageUrl = ipModel.ServerAddress;
+            localStorage.setItem('url', this.packageUrl);
+        } catch (e) {
+
+        }
+    }
 }
