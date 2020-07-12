c1ca35f34378e4e523338776bf5b20c5fafa0c41
pedram poormehr <p.poormehr@gmail.com>
2020-07-12 16:45:46 +0430

    is first login ok

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AppServiceService} from "../services/app-service.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router , private service: AppServiceService) {
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.service.isLogin()){
+      this.router.navigate(['/get-ip']);
+    }
     return true;
   }
   
--- src/app/guard/ip-auth.guard.ts
+++ src/app/guard/ip-auth.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AppServiceService} from "../services/app-service.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class IpAuthGuard implements CanActivate {
+  constructor(private router: Router , private service: AppServiceService) {
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.service.isLogin()){
+      this.router.navigate(['/home'])
+    }
     return true;
   }
   
